dfdba01f4ac5ad4b7e6d0c73e7494bf0c9dd3e52
Administrator <Administrator@samsung-fec2a15>
2011-01-31 15:57:41 +0900

    zz

--- src/gitPrac.java
+++ src/gitPrac.java
@@ -1,14 +1,9 @@
-
 public class gitPrac {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		System.out.println("sooah");
 		System.out.println("haha");
 	}
-
 }
